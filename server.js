4cdee408316b0e2b8611e1c8e8c2ba283b497c3e
StarB_ <uve0508@naver.com>
2026-02-24 14:16:43 +0900

    db Port

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const path = require("path");
 const { Server } = require("socket.io");
 const http = require("http");
 const db = createClient({
-  username: "default",
-  password: process.env.DB_PW,
-  socket: {
-    host: process.env.DB_URL,
-    port: 15900,
-  },
+    username: "default",
+    password: process.env.DB_PW,
+    socket: {
+        host: process.env.DB_URL,
+        port: parseInt(process.env.DB_PORT?.toString() || "16342"),
+    },
 });
 db.on("error", (err) => console.log("Redis Client Error", err));
 (async () => {
-  await db.connect();
-  console.log("Redis connected");
+    await db.connect();
+    console.log("Redis connected");
 })();
 
 const app = express();
@@ -28,134 +28,134 @@ const port = process.env.PORT || 3000;
 const DB_URL = process.env.LUNCH_DB;
 
 io.on("connection", (socket) => {
-  // console.log("viewer connected:", socket.id);
-  socket.emit("nowClass", classIndex);
+    // console.log("viewer connected:", socket.id);
+    socket.emit("nowClass", classIndex);
 });
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/frontend/viewer.html");
+    res.sendFile(__dirname + "/frontend/viewer.html");
 });
 
 app.get("/viewer", (req, res) => {
-  res.sendFile(__dirname + "/frontend/viewer.html");
+    res.sendFile(__dirname + "/frontend/viewer.html");
 });
 
 app.get("/control", (req, res) => {
-  res.sendFile(__dirname + "/frontend/control.html");
+    res.sendFile(__dirname + "/frontend/control.html");
 });
 
 app.get("/control1", (req, res) => {
-  res.sendFile(__dirname + "/frontend/control1.html");
+    res.sendFile(__dirname + "/frontend/control1.html");
 });
 
 app.get("/control2", (req, res) => {
-  res.sendFile(__dirname + "/frontend/control2.html");
+    res.sendFile(__dirname + "/frontend/control2.html");
 });
 
 app.get("/control3", (req, res) => {
-  res.sendFile(__dirname + "/frontend/control3.html");
+    res.sendFile(__dirname + "/frontend/control3.html");
 });
 
 app.get("/admin", (req, res) => {
-  res.sendFile(__dirname + "/frontend/admin.html");
+    res.sendFile(__dirname + "/frontend/admin.html");
 });
 
 app.get("/nextClass/:i", (req, res) => {
-  const i = Number(req.params.i);
-  classIndex[i] += 1;
-  if (classIndex[i] > maxClass[i]) classIndex[i] = 1;
-  // console.log(classIndex);
-  nowClass[i] = (firstClass[i] + classIndex[i] - 1) % maxClass[i];
-  if (nowClass[i] == 0) nowClass[i] = maxClass[i];
+    const i = Number(req.params.i);
+    classIndex[i] += 1;
+    if (classIndex[i] > maxClass[i]) classIndex[i] = 1;
+    // console.log(classIndex);
+    nowClass[i] = (firstClass[i] + classIndex[i] - 1) % maxClass[i];
+    if (nowClass[i] == 0) nowClass[i] = maxClass[i];
 
-  io.emit("nowClass", classIndex);
-  io.emit("flicker", i);
-  res.send(nowClass);
+    io.emit("nowClass", classIndex);
+    io.emit("flicker", i);
+    res.send(nowClass);
 });
 
 app.get("/prevClass/:i", (req, res) => {
-  const i = Number(req.params.i);
-  classIndex[i] -= 1;
-  if (classIndex[i] <= 0) classIndex[i] = maxClass[i];
-  // console.log(classIndex);
-  nowClass[i] = (firstClass[i] + classIndex[i] - 1) % maxClass[i];
-  if (nowClass[i] == 0) nowClass[i] = maxClass[i];
+    const i = Number(req.params.i);
+    classIndex[i] -= 1;
+    if (classIndex[i] <= 0) classIndex[i] = maxClass[i];
+    // console.log(classIndex);
+    nowClass[i] = (firstClass[i] + classIndex[i] - 1) % maxClass[i];
+    if (nowClass[i] == 0) nowClass[i] = maxClass[i];
 
-  io.emit("nowClass", classIndex);
-  // io.emit("flicker", i);
-  res.send(nowClass);
+    io.emit("nowClass", classIndex);
+    // io.emit("flicker", i);
+    res.send(nowClass);
 });
 
 app.get("/getClass", (req, res) => {
-  for (var i = 0; i < 3; i++) {
-    nowClass[i] = (firstClass[i] + classIndex[i] - 1) % maxClass[i];
-    if (nowClass[i] == 0) nowClass[i] = maxClass[i];
-  }
-  res.send(nowClass);
-  console.log(nowClass);
+    for (var i = 0; i < 3; i++) {
+        nowClass[i] = (firstClass[i] + classIndex[i] - 1) % maxClass[i];
+        if (nowClass[i] == 0) nowClass[i] = maxClass[i];
+    }
+    res.send(nowClass);
+    console.log(nowClass);
 });
 
 app.get("/read", async (req, res) => {
-  const result = await db.get("settings");
+    const result = await db.get("settings");
 
-  const data = result ? JSON.parse(result) : null;
-  _data = data;
-  firstClass = _data.firstClass;
-  maxClass = _data.classAmount;
+    const data = result ? JSON.parse(result) : null;
+    _data = data;
+    firstClass = _data.firstClass;
+    maxClass = _data.classAmount;
 
-  // console.log(`1: ${JSON.stringify(_data)} / ${firstClass} / ${maxClass}`);
-  res.json(_data);
+    // console.log(`1: ${JSON.stringify(_data)} / ${firstClass} / ${maxClass}`);
+    res.json(_data);
 });
 
 app.get("/write/:v", (req, res) => {
-  const value = req.params.v;
-  // console.log(value);
+    const value = req.params.v;
+    // console.log(value);
 
-  const valueJson = JSON.parse(value);
-  const amount = valueJson.classAmount;
-  const first = valueJson.firstClass;
-  classIndex = [1, 1, 1];
-  firstClass = first;
-  maxClass = amount;
-  nowClass = first;
+    const valueJson = JSON.parse(value);
+    const amount = valueJson.classAmount;
+    const first = valueJson.firstClass;
+    classIndex = [1, 1, 1];
+    firstClass = first;
+    maxClass = amount;
+    nowClass = first;
 
-  const forParse = JSON.stringify({
-    "classAmount": maxClass,
-    "firstClass": firstClass,
-  });
-  _data = JSON.parse(forParse);
+    const forParse = JSON.stringify({
+        "classAmount": maxClass,
+        "firstClass": firstClass,
+    });
+    _data = JSON.parse(forParse);
 
-  console.log(`asdf: ${classIndex}`);
-  io.emit("nowClass", nowClass);
-  write_db(_data);
+    console.log(`asdf: ${classIndex}`);
+    io.emit("nowClass", nowClass);
+    write_db(_data);
 });
 
 async function write_db(
-  _data = `{
+    _data = `{
     classAmount: [10, 10, 10],
     firstClass: [1, 1, 1],
-  };`
+  };`,
 ) {
-  await db.set("settings", JSON.stringify(_data));
+    await db.set("settings", JSON.stringify(_data));
 }
 
 app.get("/getMaxClass", (req, res) => {
-  res.json(JSON.parse(`{"max":[${maxClass}], "first":[${firstClass}]}`));
+    res.json(JSON.parse(`{"max":[${maxClass}], "first":[${firstClass}]}`));
 });
 
 app.get("/playSound", (req, res) => {
-  io.emit("playSound", 0);
-  res.json("{'m':'DONE!'}");
+    io.emit("playSound", 0);
+    res.json("{'m':'DONE!'}");
 });
 
 app.get("/getIndex", (req, res) => {
-  res.send(classIndex);
+    res.send(classIndex);
 });
 
 server.listen(port, () => {
-  console.log(`server is listening at localhost:${port}`);
+    console.log(`server is listening at localhost:${port}`);
 });
 
 //----------------------------------------------------------//
